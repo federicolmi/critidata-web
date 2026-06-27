fdba3dfd4f47be872276604df38cf4914cb46416
Lami Federico <federicolami@yahoo.com>
2026-06-27 20:14:33 -0300

    fix: sync Android/iOS via API igual que web

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bdf7eecc1b61cf99b95f208053be4eb2
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "97c3a86400395b69f3e929ba491740d0",
+"flutter_bootstrap.js": "719e043a73b8640c644ce780db3ea921",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
