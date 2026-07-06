09714c68689de96202df2f3e121ddd30358dbf86
Lami Federico <federicolami@yahoo.com>
2026-07-06 16:46:38 -0300

    feat: explicit back button on paciente_detalle_screen

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -28,14 +28,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bdf7eecc1b61cf99b95f208053be4eb2
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "b459d3ce20b08b47db02f715c38b7b1a",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "70c94d29f0563a522314bdd5136620d2",
+"flutter_bootstrap.js": "6b457d8e4669d28dee5b9997db1f84f2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "4ea20b29f8ee6ef8404bb925721475b3",
 "/": "4ea20b29f8ee6ef8404bb925721475b3",
-"main.dart.js": "29366d3983f3f9fc5eb2df16b8a6f966",
+"main.dart.js": "2c12cda04e7560543a76ba9f23772385",
 "manifest.json": "df7b5172b36259d75ce119e79a06df5d",
 "version.json": "4c9a05cc0d169883a77368cc5e0736ca"};
 // The application shell files that are downloaded before a service worker can
